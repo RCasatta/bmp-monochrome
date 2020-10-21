9a3b44060852db2acd70ab8423f838f15738773b
Riccardo Casatta <riccardo@casatta.it>
2020-10-21 18:12:55 +0200

    add jobs var to fuzz scripts

--- fuzz/fuzz_write.sh
+++ fuzz/fuzz_write.sh
@@ -1,2 +1,3 @@
+export JOBS=${JOBS:=1}
 cp ../test_bmp/* corpus/fuzz_target_write/
-cargo +nightly fuzz run fuzz_target_write -- -rss_limit_mb=4096
+cargo +nightly fuzz run fuzz_target_write -- -rss_limit_mb=4096 -jobs=$JOBS
